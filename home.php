78ec2e9ef3525afc6a62bb655e962ff8f7a614b8
Manikanta Addekari <156527201+mraddekarimani@users.noreply.github.com>
2024-09-21 06:27:54 +0000

    Add files via upload

---
+++ home.php
@@ -0,0 +1,14 @@
+<div class="page-title">KNOWLEDGE BRIDGE</div>
+<div>"Welcome to Knowledge Bridge, bridging the gap between knowledge and achievement."</div>
+<hr>
+<div>
+<b>Welcome to Knowledge Bride – Bridging Learning and Success!</b> <br><br>
+
+At Knowledge Bride, we are committed to helping students stay on track with their academic journey by providing an efficient and user-friendly attendance management platform. Our goal is to bridge the gap between attendance and achievement, ensuring that every student has the tools to stay consistent, engaged, and informed.
+
+Whether you’re a student striving for excellence or a faculty member looking to streamline attendance tracking, Knowledge Bride offers the perfect solution. By simplifying the process, we enable you to focus on what truly matters – learning and growing.
+
+Join us in building a bridge to knowledge, one class at a time!
+
+
+</div>
